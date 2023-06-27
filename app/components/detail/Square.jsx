4f8d483c645d63ad67497cfd856305ea38e54cec
Hoàng Quân <lehoangquanit@gmail.com>
2023-06-27 11:41:15 +0700

    comment Image not include src

--- app/components/detail/Square.jsx
+++ app/components/detail/Square.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 const Square = ({ img }) => {
     return (
         <div className='aspect-square as lg:w-[610px] rounded-[20px] bg-slate-300'>
-            {img && <Image
+            {/* {img && <Image
                 priority
                 src={img}
                 fill
                 alt="Arrow Left"
-            />}
+            />} */}
         </div>
     )
 }
--- app/components/detail/VerticalImage.jsx
+++ app/components/detail/VerticalImage.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const VerticalImage = ({ img }) => {
     return (
         <div className='lg:w-[400px] lg:h-[800px] rounded-[20px] bg-slate-300'>
-            {img && <Image
+            {/* {img && <Image
                 priority
                 src={img}
                 fill
                 alt="Arrow Left"
-            />}
+            />} */}
         </div>
     )
 }
