332a72bf7ebbdeaeec5358dbf8de26ddfd4c1dc7
Hoàng Quân <lehoangquanit@gmail.com>
2023-07-04 15:35:36 +0700

    #

--- app/contact/page.js
+++ app/contact/page.js
@@ -27,9 +27,9 @@ const Contact = () => {
 
         <div className='background absolute w-[60%] aspect-video top-[106px] right-0 p-[20px] font-third text-base font-light'>
           <h2 className='font-primary font-bold text-3xl'>DROP ME A MESSAGE</h2>
-          <p className='mt-[80px] min-w-[1920px]:mt-[122px] mb-[24px]'>Let's create great work. Together.</p>
+          <p className='mt-[80px] min-w-[1920px]:mt-[122px] mb-[24px]'>Let&apos;s create great work. Together.</p>
           <p>
-            Are you searching for a professional designer and creative strategist to effectively transform your brand? Let's team up and make it happend!Say goodbye to long meetings, over budget, grumpy process when working with agencies. My network and I can provide more effective communication. Deliver a better product, and simply faster.To discover how I can assist you, please explore my services here.Get in touch with me at  caosonwork@gmail.com or drop me a line on Instagram. I'll respond promptly.</p>
+            Are you searching for a professional designer and creative strategist to effectively transform your brand? Let&apos;s team up and make it happend!Say goodbye to long meetings, over budget, grumpy process when working with agencies. My network and I can provide more effective communication. Deliver a better product, and simply faster.To discover how I can assist you, please explore my services here.Get in touch with me at  caosonwork@gmail.com or drop me a line on Instagram. I&apos;ll respond promptly.</p>
 
           <div className='flex justify-between items-center mt-[80px] min-w-[1920px]:mt-[122px]'>
             <ul>
